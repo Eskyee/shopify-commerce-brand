bf2a3e05d2610251385d0ed92cd015867624bec0
Esky33 <djescaba@icloud.com>
2022-08-13 05:37:57 +0100

    update cart info

--- site/pages/cart.tsx
+++ site/pages/cart.tsx
@@ -59,7 +59,7 @@ export default function Cart() {
               Your cart is empty
             </h2>
             <p className="text-accent-6 px-10 text-center pt-2">
-              Don't miss out on the latest drops - junglelab.net.
+              Don't miss out on the latest drops - junglelab.net
             </p>
           </div>
         ) : error ? (
